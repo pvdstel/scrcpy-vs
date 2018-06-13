c31eae5601fc4fdf0587efa8fb2559e88807acce
Paul <pvdstel@users.noreply.github.com>
2018-06-13 11:40:38 +0200

    Update assembly information

--- scrcpy-vs/Properties/AssemblyInfo.cs
+++ scrcpy-vs/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("scrcpy Visual Studio")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("scrcpy for Visual Studio")]
+[assembly: AssemblyDescription("A scrcpy wrapper for Visual Studio.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("A scrcpy for Visual Studio.")]
+[assembly: AssemblyCompany("pvdstel")]
+[assembly: AssemblyProduct("A scrcpy wrapper for Visual Studio.")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
